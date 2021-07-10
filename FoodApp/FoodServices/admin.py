24d83c024ffb0f03c39ff0972189c9376d7fb47b
Swarnabha Roy <35457687+swarn4399@users.noreply.github.com>
2021-07-10 11:56:21 +0530

    Delete admin.py

--- FoodApp/FoodServices/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Address,Order,FoodProvider,FoodSeeker,Messages
-# Register your models here.
-admin.site.register(Address)
-admin.site.register(Order)
-admin.site.register(FoodProvider)
-admin.site.register(FoodSeeker)
-admin.site.register(Messages)
